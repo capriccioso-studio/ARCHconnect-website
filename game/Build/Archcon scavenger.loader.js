98388c2a620604d462fd6fd5a38a9d8d435b1b83
dnaniel213 <dnaniel213@gmail.com>
2023-10-13 13:51:13 +0800

    - fuck

--- game/Build/Archcon scavenger.loader.js
+++ game/Build/Archcon scavenger.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 46364745,
+    wasmFileSize: 46364462,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
